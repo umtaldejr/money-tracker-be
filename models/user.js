712081b348e0bced596b38eb6e0f3010fb8565f2
Exupério Lédo Silva Júnior <uspjr.95@hotmail.com>
2022-06-21 18:31:03 -0300

    Create User model

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    // eslint-disable-next-line no-unused-vars
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    password: DataTypes.STRING,
+    email: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
