5579fff8156dff2b1bd64404748305dff65c0051
Exupério Lédo Silva Júnior <uspjr.95@hotmail.com>
2022-06-22 14:31:07 -0300

    Hash password before saving it to DB

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const validateSchema = require('../middlewares/validate-schema');
 const { User } = require('../models/index');
 const { userSchema } = require('../validators/user');
@@ -7,7 +8,9 @@ const router = express.Router();
 
 router.post('/', validateSchema(userSchema), async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.create({ email, password });
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+  const user = await User.create({ email, password: hash });
   res.send(user);
 });
 
